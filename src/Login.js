43625cafdd80a8035a7bd70abae4ef391affd555
jhundal01 <jhundal@oswego.edu>
2019-10-05 12:56:00 -0400

    updated websocket

--- src/Login.js
+++ src/Login.js
@@ -2,27 +2,29 @@ import React, { Component } from "react";
 import { GoogleLogin } from "react-google-login";
 import config from "./config.json";
 
-const URL = "ws://localhost:3030";
+const URL = "ws://localhost:8080/test/websocketendpoint";
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       direct: false,
-      return: ""
+      message: ""
     };
   }
   ws = new WebSocket(URL);
   componentDidMount() {
+    const { direct, message } = this.state;
     this.ws.onopen = () => {
       // on connecting, do nothing but log it to the console
       console.log("connected");
     };
     this.ws.onmessage = evt => {
       // on receiving a message, add it to the list of messages
-    //   const message = JSON.parse(evt.data);
       console.log("Recieved:", evt.data);
-      //   this.addMessage(message);
+      // this.setState({
+      //   message: evt.data
+      // });
     };
     this.ws.onclose = () => {
       console.log("disconnected");
@@ -38,12 +40,12 @@ class Login extends Component {
   //   };
 
   signup = res => {
-    const { direct, data } = this.state;
-    console.log("Sent:", res.Zi);
+    const { direct, message } = this.state;
+    console.log("Sent:", res);
     this.setState({
       direct: true
     });
-    this.ws.send(JSON.stringify(res.Zi));
+    this.ws.send(JSON.stringify(res));
   };
   render() {
     const { direct } = this.state;
